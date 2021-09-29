f76250291dcf45f7747a813fda3749963fc1d7b0
Savannah Jackson <savannahjackson33@gmail.com>
2021-09-28 21:54:12 -0400

    third commit

--- FacebookConsole/Program.cs
+++ FacebookConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FacebookLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace FacebookConsole
     {
         static void Main(string[] args)
         {
+            //set all values in constructor; 
+            // Vehicle firstVehicle = new Vehicle(); - create a new instance
+            FacebookLibrary fb = new FacebookLibrary();
+
+            fb.FirstName = "Savannah";
+            fb.LastName = "Jackson";
+            fb.FavoriteMovieGenre= fb.FavoriteMovieGenre.Comedy;
+            fb.AgeInYears = 24;
+            fb.FavoriteMusic = "R&B";
+
+
+            //empty because of empty constructor; allows user to set values
         }
     }
 }
--- FacebookLibrary/FacebookLibrary.cs
+++ FacebookLibrary/FacebookLibrary.cs
@@ -6,24 +6,15 @@ using System.Threading.Tasks;
 
 namespace FacebookLibrary
 {
+    public enum FavoriteMovieGenre { Action, Comedy, Romance, Documentary, Horror }
+
     public class FacebookLibrary
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string FavoriteMovies { get; set; }
+        public FavoriteMovieGenre Genre { get; set; }
         public int AgeInYears { get; set; }
         public string FavoriteMusic { get; set; }
-
-
-        public enum FavoriteMovies(string Action, string Comedy, string Romance, string Documentary, string Horror)
-        {
-            Action = action;
-            Comedy = comedy;
-            Romance = romance;
-            Documentary = documentary;
-            Horror = horror;
-
-        }
 
         public string FullName
         {
@@ -35,11 +26,29 @@ namespace FacebookLibrary
         {
             get
             {
+                DateOfBirth = dateOfBirth;
                 TimeSpan ageSpan = DateTime.Now - dateOfBirth;
                 double totalAgeInyears = ageSpan.TotalDays / 365.25;
                 return Convert.ToInt32(Math.Floor())
-                }
+            }
+        }
+
+        public FacebookLibrary()
+        {
+
+        }
+
+        public FacebookLibrary(string firstName, lastName, FavoriteMovieGenre, favoriteMusic)
+        {
+            //set equal to props (parameters)
+            FirstName = firstName;
+            LastName = lastName;
+            FavoriteMusic = favoriteMusic;
+
+
+
         }
 
     }
+
 }
